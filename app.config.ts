6d20d03c4d2b7eda805d3494c418e476f2f2e4ad
mp1707 <marco.preuss1707@gmail.com>
2025-11-18 00:09:44 +0100

    version

--- app.config.ts
+++ app.config.ts
@@ -12,7 +12,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: IS_DEV ? "MacroLoop D" : IS_PREVIEW ? "MacroLoop P" : "MacroLoop",
   slug: "gainslog",
-  version: "1.0.2",
+  version: "1.0.3",
   orientation: "portrait",
   newArchEnabled: true,
   scheme: SCHEME,
